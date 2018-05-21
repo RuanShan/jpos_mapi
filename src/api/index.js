4c6fc119712e730b6f6f13357fe2637553a2c6a3
STM32King <30915620+STM32King@users.noreply.github.com>
2018-05-21 10:58:48 +0800

    去汪的店后

--- src/api/index.js
+++ src/api/index.js
@@ -7,32 +7,32 @@ import path from "path";
 import { path as rootPath } from 'app-root-path';
 
 export default ({ config, db }) => {
-	let api = Router();
+    let api = Router();
 
-	// perhaps expose some API metadata at the root
-	api.get('/', (req, res) => {
-		res.json({ version });
-	});
+    // perhaps expose some API metadata at the root
+    api.get('/', (req, res) => {
+        res.json({ version });
+    });
 
-	api.get('/users/*', (req, res) => {
+    api.get('/users/*', (req, res) => {
 
-		let filePath = path.join( rootPath , config.public , req.originalUrl + '.json');
-	  fs.readFile(filePath, function(err, contents) {
-			console.log(filePath, contents )
-	    res.json( JSON.parse(contents) );
-	    res.end();
-  	});
-	});
+        let filePath = path.join(rootPath, config.public, req.originalUrl + '.json');
+        fs.readFile(filePath, function(err, contents) {
+            console.log(filePath, contents)
+            res.json(JSON.parse(contents));
+            res.end();
+        });
+    });
 
-	api.all('/api/*', (req, res) => {
+    api.all('/api/*', (req, res) => {
 
-		let filePath = path.join( rootPath , config.public , req.originalUrl + '.json');
-	  fs.readFile(filePath, function(err, contents) {
-			console.log(filePath, contents )
-	    res.json( JSON.parse(contents) );
-	    res.end();
-  	});
-	});
+        let filePath = path.join(rootPath, config.public, req.originalUrl + '.json');
+        fs.readFile(filePath, function(err, contents) {
+            console.log(filePath, contents)
+            res.json(JSON.parse(contents));
+            res.end();
+        });
+    });
 
-	return api;
+    return api;
 }
